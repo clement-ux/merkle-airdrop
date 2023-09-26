ce394e83bfcbe7cc5307954664356f16e4f3b9b7
clement-ux <clemmoller@gmail.com>
2023-09-26 14:24:15 +0200

    test: add test for claiming airdrop

--- test/MerkleTreeHelper.sol
+++ test/MerkleTreeHelper.sol
@@ -6,7 +6,6 @@ import "forge-std/Test.sol";
 
 contract MerkleTreeHelper is Test {
     function getProof(address user) public returns (uint256, uint256, bytes32[] memory) {
-        //returns(bytes32) {
         string[] memory inputs = new string[](3);
         inputs[0] = "node";
         inputs[1] = "merkletree-script/merkleProof.js";
--- test/SwellAirdropClaim.t.sol
+++ test/SwellAirdropClaim.t.sol
@@ -15,6 +15,8 @@ contract SwellAirdropClaimTest is MerkleTreeHelper {
     address public immutable alice = makeAddr("alice");
     address public immutable bob = makeAddr("bob");
 
+    uint256 public constant TOTAL_AIRDROP = 1_000_000e18;
+
     modifier setMerkelRoot() {
         claim.setMerkleRoot(getRoot());
         _;
@@ -25,7 +27,7 @@ contract SwellAirdropClaimTest is MerkleTreeHelper {
         _;
     }
 
-    function test_ClaimAirDrop_Alice() public setMerkelRoot sendTokenToMerkle(30e18) {
+    function test_ClaimAirDrop_Alice() public setMerkelRoot sendTokenToMerkle(TOTAL_AIRDROP) {
         // Check before
         assertEq(swell.balanceOf(alice), 0);
 
@@ -34,9 +36,86 @@ contract SwellAirdropClaimTest is MerkleTreeHelper {
 
         // Claim Airdrop
         vm.prank(alice);
-        claim.claimAirDrop({proof: proof, index: index, amount: amount});
+        claim.claimAirDrop(proof, index, amount);
 
         // Check after
-        assertEq(swell.balanceOf(alice), 10e18);
+        assertEq(swell.balanceOf(alice), amount);
+    }
+
+    function test_RevertWhen_NoTokenOnContract() public setMerkelRoot {
+        // Get Proof
+        (uint256 index, uint256 amount, bytes32[] memory proof) = getProof(alice);
+
+        vm.expectRevert();
+        vm.prank(alice);
+        claim.claimAirDrop(proof, index, amount);
+    }
+
+    function test_RevertWhen_AlreadyClaimed() public setMerkelRoot sendTokenToMerkle(TOTAL_AIRDROP) {
+        // Get Proof
+        (uint256 index, uint256 amount, bytes32[] memory proof) = getProof(alice);
+
+        // Claim Airdrop
+        vm.prank(alice);
+        claim.claimAirDrop(proof, index, amount);
+
+        // Claim Again
+        vm.expectRevert("Already claimed");
+        vm.prank(alice);
+        claim.claimAirDrop(proof, index, amount);
+    }
+
+    function test_RevertWhen_ProofIsInvalid() public setMerkelRoot {
+        // Get Proof
+        (uint256 index, uint256 amount, bytes32[] memory proof) = getProof(alice);
+        // Modify proof
+        proof[0] = bytes32("123");
+
+        // Claim Airdrop
+        vm.expectRevert("Invalid proof");
+        vm.prank(alice);
+        claim.claimAirDrop(proof, index, amount);
+    }
+
+    function test_RevertWhen_AmountIsInvalid() public setMerkelRoot {
+        // Get Proof
+        (uint256 index, uint256 amount, bytes32[] memory proof) = getProof(alice);
+        // Modify amount
+        amount -= 1;
+
+        // Claim Airdrop
+        vm.expectRevert("Invalid proof");
+        vm.prank(alice);
+        claim.claimAirDrop(proof, index, amount);
+
+        // Modify amount
+        amount += 2;
+
+        // Claim Airdrop
+        vm.expectRevert("Invalid proof");
+        vm.prank(alice);
+        claim.claimAirDrop(proof, index, amount);
+    }
+
+    function test_RevertWhen_IndexIsInvalid() public setMerkelRoot {
+        // Get Proof
+        (uint256 index, uint256 amount, bytes32[] memory proof) = getProof(alice);
+        // Modify index
+        index += 1;
+
+        // Claim Airdrop
+        vm.expectRevert("Invalid proof");
+        vm.prank(alice);
+        claim.claimAirDrop(proof, index, amount);
+    }
+
+    function test_RevertWhen_WrongCaller() public setMerkelRoot sendTokenToMerkle(TOTAL_AIRDROP) {
+        // Get Proof
+        (uint256 index, uint256 amount, bytes32[] memory proof) = getProof(alice);
+
+        // Claim Airdrop
+        vm.expectRevert("Invalid proof");
+        vm.prank(bob);
+        claim.claimAirDrop(proof, index, amount);
     }
 }
