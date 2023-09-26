6631fa930354692c7c8fcfa55d47d9a7e851f065
clement-ux <clemmoller@gmail.com>
2023-09-26 15:46:34 +0200

    feat: generate random list of users

---
+++ merkletree-script/randomValuesGenerator.js
@@ -0,0 +1,45 @@
+function getRandomArbitrary(min, max) {
+    return Math.pow(10, Math.random() * (Math.log10(max) - Math.log10(min)) + Math.log10(min));
+}
+
+function generateRandomHexString(length) {
+    let result = '0x';
+    const characters = '0123456789abcdef';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters.charAt(randomIndex);
+    }
+
+    return result;
+}
+
+function generateRandomData(numTimes) {
+    const dataArray = [];
+
+    for (let i = 0; i < numTimes; i++) {
+        const firstPart = generateRandomHexString(40);
+        const secondPart = i.toString();
+        const thirdPart = parseInt(getRandomArbitrary(1e15, 1e22)).toString();
+
+        const textToPrint = [];
+        textToPrint.push(firstPart);
+        textToPrint.push(secondPart);
+        textToPrint.push(thirdPart);
+
+
+        dataArray.push(textToPrint);
+    }
+
+    // Add some hardcoded values at random places
+    dataArray[getRandomInt(numTimes - 1)][0] = "0x328809Bc894f92807417D2dAD6b7C998c1aFdac6"; // makeAddr("alice")
+    dataArray[getRandomInt(numTimes - 1)][0] = "0x1D96F2f6BeF1202E4Ce1Ff6Dad0c2CB002861d3e"; // makeAddr("bob")
+
+    return dataArray;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+export default generateRandomData;
